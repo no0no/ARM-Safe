5e74954e46bf662036ed2288e3f34bec0604f9a7
no no <gageaschenbrenner@gmail.com>
2016-11-01 17:29:31 -0700

    Add files via upload
    Reading String from file working
    Added more error handling
    Have not figured out the way to access individual characters from memory

--- Part-1.s
+++ Part-1.s
@@ -6,6 +6,9 @@
 
 Main:
 	BL	OpenInput
+	BL	ReadString
+	BL	PrintString
+	BL	CloseFiles
 
 OpenInput:
 	ldr	r0,=InFileName
@@ -15,6 +18,21 @@ OpenInput:
 	ldr	r1,=InFileHandle			@ Memory [r1] is for input file handle
 	str	r0,[r1]
 	bx	lr					@ Branches back to where it was called (i.e Main)
+	
+ReadString:
+	ldr	r0,[r1]
+	mov	r2,#80
+	ldr	r3,=InputString
+	str	r1,[r3]
+	swi	SWI_RdStr
+	bcs	ReadError
+	bx	lr
+	
+PrintString:
+	mov	r0,#StdOut
+	ldr	r1,[r3]
+	swi	SWI_PrStr
+	bx	lr
 
 Encrypt:
 	
@@ -24,12 +42,19 @@ NoInFileFound:
 	mov	r0,#StdOut
 	ldr	r1,=NoInFileErr
 	swi	SWI_PrStr
-	B	Exit
+	b	Exit
+	
+ReadError:
+	mov	r0,#StdOut
+	ldr	r1,=ReadErr
+	swi	SWI_PrStr
+	b	CloseFiles
 
 @ ==== Closes input and output file ==== @
 CloseFiles:
 	ldr	r0,[r1]
 	swi	SWI_Close
+	b	Exit
 
 Exit:
 	SWI	SWI_Exit
@@ -40,6 +65,10 @@ InFileHandle:	.word 0
 
 @ ==== Strings ==== @
 NoInFileErr:	.asciz "Fatal Error: Unable to find input.txt\r\n"
+ReadErr:	.asciz "Fatal Error: Unable to read number from input.txt"
+
+@ ==== Memory Addresses ==== @
+InputString:	.word 0
 
 @ ==== SWI Statements ==== @
 	.equ	StdOut,		1
