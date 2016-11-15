d0eec55ecfbfe772160808203402f36bbdc4e493
no no <gageaschenbrenner@gmail.com>
2016-11-15 00:28:40 -0800

    Add files via upload
    just need to make it output to output.txt

--- Part-1.s
+++ Part-1.s
@@ -5,45 +5,75 @@
 @ encryption to an output file
 
 Main:
-	BL	OpenInput
-	BL	ReadString
-	BL	PrintString
-	BL	CloseFiles
+	bl	OpenInput
+	bl	ReadInt
+	bl	ReadString
+	bl	Encrypt
+	bl	PrintString
+	bl	CloseFiles
 
 OpenInput:
 	ldr	r0,=InFileName
 	mov	r1,#0
 	swi	SWI_Open
 	bcs	NoInFileFound
-	ldr	r1,=InFileHandle			@ Memory [r1] is for input file handle
-	str	r0,[r1]
+	ldr	r2,=InFileHandle			@ Memory [r2] is for input file handle
+	str	r0,[r2]
+
 	bx	lr					@ Branches back to where it was called (i.e Main)
 	
+ReadInt:
+	ldr	r0,[r2]
+	swi	SWI_RdInt
+	bcs	ReadError
+	ldr	r5,=InputNumber
+	str	r0,[r5]
+	ldr	r5,[r5]
+
+	bx	lr
+	
 ReadString:
-	ldr	r0,[r1]
+	ldr	r0,[r2]
+	ldr	r1,=InputString
 	mov	r2,#80
-	ldr	r3,=InputString
-	str	r1,[r3]
 	swi	SWI_RdStr
 	bcs	ReadError
+
 	bx	lr
 	
-PrintString:
-	mov	r0,#StdOut
-	ldr	r1,[r3]
-	swi	SWI_PrStr
+@ ==== Encrypts the byte with a shift value determined by user ==== @	
+Encrypt:
+	ldrb	r4,[r1], #1
+	cmp	r4,#0
+	addne	r4,r4,r5
+	strb	r4,[r1, #-1]
+	bne	Encrypt
+
 	bx	lr
 
-Encrypt:
+PrintString:
+	mov	r0,#StdOut
+	ldr	r1,=InputString
+	swi	SWI_PrStr
+
+	bx	lr
+
+OpenOutput:
+	ldr	r0,=OutFileName
+	mov	r1,#1
+	swi	SWI_Open
+	ldr	r1,=OutFileHandle
+	str	r0,[r1]
 	
+	bx	lr
 
 @ ==== Input File Error ==== @	
 NoInFileFound:
 	mov	r0,#StdOut
-	ldr	r1,=NoInFileErr
+	ldr	r1,[r3]
 	swi	SWI_PrStr
 	b	Exit
-	
+
 ReadError:
 	mov	r0,#StdOut
 	ldr	r1,=ReadErr
@@ -52,7 +82,7 @@ ReadError:
 
 @ ==== Closes input and output file ==== @
 CloseFiles:
-	ldr	r0,[r1]
+	ldr	r0,[r2]
 	swi	SWI_Close
 	b	Exit
 
@@ -62,16 +92,20 @@ Exit:
 @ ==== File Components ==== @
 InFileName:	.asciz "input.txt"
 InFileHandle:	.word 0
+OutFileName:	.asciz "output.txt"
+OutFileHandle:	.word 0
 
 @ ==== Strings ==== @
 NoInFileErr:	.asciz "Fatal Error: Unable to find input.txt\r\n"
 ReadErr:	.asciz "Fatal Error: Unable to read number from input.txt"
 
 @ ==== Memory Addresses ==== @
-InputString:	.word 0
+InputString:	.skip 80
+InputNumber:	.word 0
 
 @ ==== SWI Statements ==== @
 	.equ	StdOut,		1
+	.equ	SWI_RdInt,	0x6c
 	.equ	SWI_RdStr,	0x6a
 	.equ	SWI_PrStr,	0x69
 	.equ	SWI_Open,	0x66
